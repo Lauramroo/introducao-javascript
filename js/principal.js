95f62c56f58f1e71d46b025f983d3bbf1f9612b3
Lauramroo <rodrigues.oliveira.laura@escola.pr.gov.br>
2025-11-05 11:41:08 -0300

    iimc

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,22 @@
+var titulo = document.querySelector(".título");
+titulo.textContent = "Aparecida Nutricionista"; 
+
+/* IMC = massa/altura^2    
+var paciente = document.querySelector("#primeiro-paciente");
+var tdpeso = tdPeso.textContent;
+var peso  = tdPeso.textContent; 
+var tdAltura = document.querySelector(" .info-altura");
+var altura = tdAltura.textContent
+
+var imc = peso / (alura*altura);
+console.log(paciente); //tr
+console.log(tdPeso); // td que tem peso
+console.log(tdPeso); // td que tem altura 
+console.log(peso); //valor do peso
+console.log(altura); //valor da altura
+console.log(imc);
+
+
 <script> 
 			var titulo = document.querySelector(".titulo");
 
