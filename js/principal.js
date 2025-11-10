4a9a246b1c48dbf67bac9e1f357bce3ec18688a6
Lauramroo <rodrigues.oliveira.laura@escola.pr.gov.br>
2025-11-10 09:22:45 -0300

    imc na pagina

--- js/principal.js
+++ js/principal.js
@@ -1,28 +1,19 @@
-var titulo = document.querySelector(".título");
-titulo.textContent = "Aparecida Nutricionista"; 
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
 
-/* IMC = massa/altura^2    
+/*  IMC = massa/altura^2    */
 var paciente = document.querySelector("#primeiro-paciente");
-var tdpeso = tdPeso.textContent;
-var peso  = tdPeso.textContent; 
-var tdAltura = document.querySelector(" .info-altura");
-var altura = tdAltura.textContent
+var tdPeso = document.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+var tdAltura = document.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+var imc = peso / (altura*altura);
 
-var imc = peso / (alura*altura);
-console.log(paciente); //tr
-console.log(tdPeso); // td que tem peso
-console.log(tdPeso); // td que tem altura 
-console.log(peso); //valor do peso
-console.log(altura); //valor da altura
-console.log(imc);
+console.log(paciente);  //tr
+console.log(tdPeso);    // td que tem peso
+console.log(tdAltura);  //td que tem altura
+console.log(peso);      //valor do peso
+console.log(altura);    //valor da altura
 
-
-<script> 
-			var titulo = document.querySelector(".titulo");
-
-			//console.log(titulo);
-			//console.log(titulo.textContent);
-
-			titulo.textContent = "Aparecida Nutricionista";
-
-		</script>
+var tdImc = paciente.querySelector(".info-imc");
+tdImc.textContent = imc;
